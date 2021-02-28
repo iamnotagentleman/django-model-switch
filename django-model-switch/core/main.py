e0c9e3be3114f9a4acb0523c1b7a4419cc921a4d
iamnotagentleman <realveli24@gmail.com>
2021-02-28 12:52:33 +0300

    ABC class created for Generators.

--- django-model-switch/core/main.py
+++ django-model-switch/core/main.py
@@ -1,2 +1,27 @@
-class BaseGenerator(object):
-    ...
+from abc import ABCMeta, abstractmethod
+
+
+class BaseGenerator(ABCMeta):
+    @abstractmethod
+    def get_classname(cls):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_prefix(cls):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_attr(cls):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_suffix(cls):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def stream_params(cls):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def generate_field(cls):
+        raise NotImplementedError()
